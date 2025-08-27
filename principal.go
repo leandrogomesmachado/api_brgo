a20ec56d1eb1f5f47b83cbfce54fcb83d57d2443
Leandro Machado <sr.machado@gmail.com>
2025-08-27 11:18:10 -0300

    Wrappers para o pacote

---
+++ principal.go
@@ -0,0 +1,7 @@
+// This is a wrapper file for the brgo implementation
+// It allows the Go module system to recognize the package
+
+package principal
+
+// The actual implementation is in the .brgo files
+// This file exists to make the Go module system work with our custom extensions
